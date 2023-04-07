fadef3ed3dfb4b27dc64f93a5c3105a5aa117a09
thodnev <thodnev@xinity.dev>
2023-04-08 00:53:24 +0300

    feat(repo): 2nd practice (fixed view)

--- app.py
+++ app.py
@@ -1,9 +1,47 @@
 from functools import lru_cache
 from flask import Flask, abort, request, Response
+from flask import Blueprint, render_template
 
 from statapi import methods
 
 app = Flask(__name__)
+
+
+# (!) Warning: normally static files *MUST* be served by
+#     web server (e.g. Nginx), NOT Flask
+#     But Flask provides a way to serve statics (if needed)
+#     for simple projects, development and testing purposes
+# (!) Flask adds default /static route to serve files from 'static' dir
+# Custom folder for assets (also normally must be served by web server)
+assets = Blueprint('assets', __name__, static_folder='assets')
+app.register_blueprint(assets, url_prefix='/')
+
+
+@app.route('/memory')
+def memory():
+    mem = methods['virtual_memory'](format=None)
+    app.logger.debug('Got memory data:\n\t %r', mem)
+    return render_template('memory.html.jinja',
+                           pagetitle='Memory statistics',
+                           statname='memory',
+                           mem=mem
+                           )
+
+# TODO: add a template rendering route where progressbar gets filled
+#       on client-side, with requests done using js fetch() method.
+#       Some basics can be filled on server-side though, like page title
+# TODO(extra): try to modify js:
+#       - to make server return server-side rendered page
+#       - with js dynamically updating its data
+# TODO(extra): try extracting common template values into jinja block in separate file
+#       and use template inheritance
+@app.route('/memory-client')
+def memory_client():
+    ...
+    # add logic here
+    ...
+    return render_template('memory-client.html')
+
 
 
 @app.route('/stats/')
